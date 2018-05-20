897453ecdd5276214236d153cbc5494669bcdeba
umed tolibi <tolibiumed@gmail.com>
2018-05-20 12:35:35 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on("connect", function() {
   console.log("Connected to the server");
-
-  socket.emit("createMessage", {
-    from: "jen@example.com",
-    text: "Hey. This is Andrew."
-  });
 });
 
 socket.on("disconnect", function() {
